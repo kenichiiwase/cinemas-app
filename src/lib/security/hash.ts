f3b2777a5fcb20222b1f85a958cdf331b6c32b07
Kenichi Iwase <44935028+kenichiiwase@users.noreply.github.com>
2021-10-04 00:41:28 +0900

    Update hash.ts

--- src/lib/security/hash.ts
+++ src/lib/security/hash.ts
@@ -1,18 +1,20 @@
 import * as crypto from "crypto";
+
 const salt = process.env.PASSWORD_SALT;
 
 if (!salt) {
   throw new Error("PASSWORD_SALTが未設定です。");
 }
 
-const digest = function (text: string) {
+const digest = function createHashPass(text: string):string {
   let hash;
 
   text += salt;
 
-  for (var i = 3; i--; ) {
+  for (let i = 3; i--;) {
     hash = crypto.createHash("sha256");
     hash.update(text);
+
     text = hash.digest("hex");
   }
   return text;
