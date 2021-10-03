97f2d9153b49cac6102fc4e922cf1e0eebf5d23f
Kenichi Iwase <44935028+kenichiiwase@users.noreply.github.com>
2021-10-04 00:37:38 +0900

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,25 +1,26 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 import hash from "../lib/security/hash";
-let router = express.Router();
+
+const router = express.Router();
 const connectionUrl = process.env.CONNECTION_URL;
 
 if (!connectionUrl) {
   throw new Error("CONNECTION_URLが未設定です。");
 }
 
-const createRegistData = function (body: any, totaluser: number, hash: string) {
+function createRegistData(body: any, totaluser: number, hash: string) {
   const datetime = new Date();
   return {
     user_id: totaluser + 1,
     email: body.email,
-    name: body.lastName + " " + body.firstName,
+    name: `${body.lastName} ${body.firstName}`,
     password: hash,
     phoneNo: body.phoneNo,
     role: "general",
     registtime: datetime,
   };
-};
+}
 
 router.get("/", (req: express.Request, res: express.Response) => {
   res.render("./user/post/user-input.ejs", { message: req.flash("message") });
@@ -45,7 +46,7 @@ router.post(
             const data = createRegistData(
               req.body,
               totaluser,
-              hash.digest(req.body.password)
+              hash.digest(req.body.password),
             );
             await db.collection("users").insertOne(data);
             res.redirect("/user/post/regist/complete");
@@ -53,7 +54,7 @@ router.post(
         } catch (error) {
           req.flash(
             "message",
-            "ユーザー情報チェック時にエラーが発生しました。"
+            "ユーザー情報チェック時にエラーが発生しました。",
           );
           res.render("./user/post/user-input.ejs", {
             message: req.flash("message"),
@@ -67,19 +68,19 @@ router.post(
   (req: express.Request, res: express.Response) => {
     req.flash(
       "message",
-      "そのユーザー(メールアドレス)は既に登録されているため、登録できません。"
+      "そのユーザー(メールアドレス)は既に登録されているため、登録できません。",
     );
     res.render("./user/post/user-input.ejs", {
       message: req.flash("message"),
     });
-  }
+  },
 );
 
 router.get(
   "/post/regist/complete",
   (req: express.Request, res: express.Response) => {
     res.render("./user/post/user-complete.ejs");
-  }
+  },
 );
 
 export default router;
