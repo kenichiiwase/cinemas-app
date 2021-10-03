3d0026b478da0201e514bdc0f43b152ae9755ad4
Kenichi Iwase <44935028+kenichiiwase@users.noreply.github.com>
2021-10-04 00:38:27 +0900

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import accountControl from "../lib/security/accountcontrol.js";
-let router = express.Router();
+import accountControl from "../lib/security/accountcontrol";
+
+const router = express.Router();
 
 router.get("/", (req: express.Request, res: express.Response) => {
   res.render("./login.ejs", { message: req.flash("message") });
@@ -11,7 +12,7 @@ router.post(
   accountControl.authenticate(),
   (req: express.Request, res: express.Response) => {
     res.render("./login.ejs", { message: req.flash("message") });
-  }
+  },
 );
 
 router.get("/login/success", (req: express.Request, res: express.Response) => {
@@ -24,7 +25,7 @@ router.post(
   (req: express.Request, res: express.Response) => {
     req.logout();
     res.redirect("/");
-  }
+  },
 );
 
 export default router;
